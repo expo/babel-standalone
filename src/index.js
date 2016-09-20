fbfe058e3e21c0dc9e2734e22f88bab26c34b50d
Charlie Cheever <ccheever@gmail.com>
2016-09-20 04:23:18 -0700

    Added exponent preset

--- src/index.js
+++ src/index.js
@@ -194,6 +194,44 @@ registerPresets({
   'stage-1': require('babel-preset-stage-1'),
   'stage-2': require('babel-preset-stage-2'),
   'stage-3': require('babel-preset-stage-3'),
+  'exponent': {
+    plugins: [
+      require('babel-plugin-transform-decorators-legacy').default,
+      require('babel-plugin-transform-do-expressions'),
+      require('babel-plugin-transform-function-bind'),
+      require('babel-plugin-transform-class-constructor-call'),
+      require('babel-plugin-transform-export-extensions'),
+      require('babel-plugin-syntax-trailing-function-commas'),
+      require('babel-plugin-transform-exponentiation-operator'),
+
+      //
+      require('babel-plugin-syntax-async-functions'),
+      require('babel-plugin-syntax-class-properties'),
+      require('babel-plugin-syntax-trailing-function-commas'),
+      require('babel-plugin-transform-class-properties'),
+      require('babel-plugin-transform-es2015-function-name'),
+      require('babel-plugin-transform-es2015-arrow-functions'),
+      require('babel-plugin-transform-es2015-block-scoping'),
+      require('babel-plugin-transform-es2015-classes'),
+      require('babel-plugin-transform-es2015-computed-properties'),
+      require('babel-plugin-check-es2015-constants'),
+      require('babel-plugin-transform-es2015-destructuring'),
+      [require('babel-plugin-transform-es2015-modules-commonjs'), { strict: false, allowTopLevelThis: true }],
+      require('babel-plugin-transform-es2015-parameters'),
+      require('babel-plugin-transform-es2015-shorthand-properties'),
+      require('babel-plugin-transform-es2015-spread'),
+      require('babel-plugin-transform-es2015-template-literals'),
+      require('babel-plugin-transform-es2015-literals'),
+      require('babel-plugin-transform-flow-strip-types'),
+      require('babel-plugin-transform-object-assign'),
+      require('babel-plugin-transform-object-rest-spread'),
+      require('babel-plugin-transform-react-display-name'),
+      require('babel-plugin-transform-react-jsx'),
+      require('babel-plugin-transform-regenerator'),
+      [require('babel-plugin-transform-es2015-for-of'), { loose: true }],
+      /* require('../transforms/transform-symbol-member'), */
+    ]
+  },
 
   // ES2015 preset with es2015-modules-commonjs removed
   // Plugin list copied from babel-preset-es2015/index.js
